5915b2314cefee97022be243f449739765407f74
mrazawordpress <muhammadrazadeveloper@gmail.com>
2020-02-21 02:21:50 +0500

    Adding Form Validation HTML file

---
+++ Form Validation/js/FormCreation.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	checkInputs();
+});
+function checkInputs(){
+	//get the values from the inputs
+	const usernameValue=username.value.trim();
+	const emailValue=email.value.trim();
+	const passwordValue=password.value.trim();
+	const password2Value=password2.value.trim();
+	
+	if(usernameValue ===''){
+		//show error
+		//add error class
+		setErrorfor(username,'username cannot be blank');
+	}else{
+		//add success class
+		setSuccessFor(username);
+	}
+}
+function setErrorfor(input,message){
+	const formControl=input.paretElement;//.form-control
+	const small=formControl.querySelector('small');
+	//add error message inside small
+	small.innerText=message;
+	//add error class
+	formControl.className='form-control error';
+}
+alert('this is js');
---
+++ Form Validation/js/form.js
@@ -0,0 +1,1 @@
+alert('external is ready')
